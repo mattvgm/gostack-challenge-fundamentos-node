bfbb8f245efce4e6305077f1c3dd1deb37764f37
Matheus V <matheusvrgm@gmail.com>
2020-04-17 00:17:42 -0300

    Challenge Done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const IncomeValue = this.transactions
+      .filter(eachTrans => eachTrans.type === 'income')
+      .map(eachIncome => eachIncome.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const OutcomeValue = this.transactions
+      .filter(eachTrans => eachTrans.type === 'outcome')
+      .map(eachOutcome => eachOutcome.value)
+      .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    const total = IncomeValue - OutcomeValue;
+    return { income: IncomeValue, outcome: OutcomeValue, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const NewTransaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+    this.transactions.push(NewTransaction);
+    return NewTransaction;
   }
 }
 
